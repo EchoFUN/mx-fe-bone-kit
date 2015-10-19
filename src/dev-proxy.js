4743bf47c8a227d577afb57005079d381d438ca9
ddchen <842914439@qq.com>
2015-10-19 10:19:43 +0800

    add pac proxy server

---
+++ src/dev-proxy.js
@@ -0,0 +1,39 @@
+import kit from 'nokit';
+
+let {
+    _
+} = kit;
+let proxy = kit.require('proxy');
+let {
+    select
+} = proxy;
+let br = kit.require('brush');
+
+let pacPath = '/pac';
+
+export default async(opts) => {
+    let app = proxy.flow();
+
+    app.push.apply(app, _.chain([
+        // provide proxy auto config.
+        // pac address is http://127.0.0.1:${opts.port}/${opts.pac}
+        // graphic tutorials: http://www.artica.fr/download/Proxy_Configuration_Mac_OSX_Leopard.pdf
+        // see https://support.apple.com/kb/PH10934?locale=en_US
+        // see https://en.wikipedia.org/wiki/Proxy_auto-config
+        select(pacPath, async($) => {
+            $.body = `function FindProxyForURL(url, host) {
+                    if (shExpMatch(host, "${opts.host}")) {
+                        return "PROXY 127.0.0.1:${opts.proxyPort}";
+                    }
+                    return "DIRECT";
+                }`;
+        }),
+
+        select(/.*/, proxy.url(`127.0.0.1:${opts.port}`))
+    ]).flatten().compact().value());
+
+    await app.listen(opts.proxyPort);
+    kit.logs('inner pac proxy server listen at:', br.cyan(opts.proxyPort));
+
+    await kit.exec(`echo "kino" | sudo -S networksetup -setautoproxyurl Wi-Fi 'http://127.0.0.1:${opts.proxyPort}/pac?_${new Date().getTime()}'`);
+};
