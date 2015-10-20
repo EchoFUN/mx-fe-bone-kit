eb864b6e233617c03aea3e90f93bb68870e2bb22
ddchen <842914439@qq.com>
2015-10-20 15:16:09 +0800

    add default router way

---
+++ src/default-router.js
@@ -0,0 +1,41 @@
+// 这只是个示例文件，不会被版本跟踪
+import kit from 'nokit';
+let proxy = kit.require('proxy');
+let serverHelper = proxy.serverHelper();
+
+let {
+    match, select
+} = proxy;
+let {
+    _
+} = kit;
+
+export default (app, opts, config) => {
+    let rawPaths = config.rawPaths;
+    let {
+        pageDev, asset,
+        src, favicon
+    } = config.paths;
+    // 默认路由服务
+    app.push.apply(app, _.chain([
+        // 入口页面路由
+        select(match('/:page'), async($) => {
+            let tpl = require(pageDev)({
+                vendor: `/${rawPaths.assetPage}/vendor.js`,
+                page: `/${rawPaths.assetPage}/${$.url.page}.js`
+            });
+
+            // 插入自动重载等工具函数到页面
+            $.body = tpl + kit.browserHelper();
+        }),
+
+        // favicon
+        select('/favicon.ico', kit.readFile(favicon)),
+
+        // 静态资源
+        [asset, src].map(path => select(`/${rawPaths.asset}`, proxy.static({
+            root: path,
+            onFile: _.ary(serverHelper.watch, 1)
+        })))
+    ]).flatten().compact().value());
+};
