04ed213149e9170de55ec0d7aaa9102049591151
ddchen <842914439@qq.com>
2015-10-20 15:17:01 +0800

    remove default router logic.

--- src/dev-server.js
+++ src/dev-server.js
@@ -4,15 +4,12 @@ import config from './public-config';
 import devproxy from './dev-proxy';
 
 let {
-    pageDev, asset, mock,
-    src, favicon
+    mock
 } = config.paths;
-let rawPaths = config.rawPaths;
-let { _ } = kit;
 let br = kit.require('brush');
 let proxy = kit.require('proxy');
-let { match, select } = proxy;
 let serverHelper = proxy.serverHelper();
+
 let opts = JSON.parse(process.argv[2]);
 
 if (opts.port === '<%= port %>')
@@ -25,45 +22,24 @@ utils.checkPort(opts.port);
 // 总入口服务
 let app = proxy.flow();
 
-app.push.apply(app, _.chain([
+// 日志和帮助服务
+app.push(
     utils.accessLog('access:'),
+    serverHelper
+);
 
-    serverHelper,
+// 载入 mock 入口点
+let isLoadMock = true;
+try {
+    require.resolve(mock);
+    kit.logs(`load module "${mock}"`);
+} catch (err) {
+    isLoadMock = false;
+    kit.logs(br.yellow(`skip module "${mock}"`));
+}
+if (isLoadMock) require(mock)(app, opts, config, utils);
 
-    // 入口页面路由
-    select(match('/:page'), async ($) => {
-        let tpl = require(pageDev)({
-            vendor: `/${rawPaths.assetPage}/vendor.js`,
-            page: `/${rawPaths.assetPage}/${$.url.page}.js`
-        });
-
-        // 插入自动重载等工具函数到页面
-        $.body = tpl + kit.browserHelper();
-    }),
-
-    // favicon
-    select('/favicon.ico', kit.readFile(favicon)),
-
-    // 静态资源
-    [asset, src].map(path => select(`/${rawPaths.asset}`, proxy.static({
-        root: path,
-        onFile: _.ary(serverHelper.watch, 1)
-    })))
-]).flatten().compact().value());
-
-(async () => {
-    // 载入 mock 入口点
-    let isLoadMock = true;
-    try {
-        require.resolve(mock);
-        kit.logs(`load module "${mock}"`);
-    } catch (err) {
-        isLoadMock = false;
-        kit.logs(br.yellow(`skip module "${mock}"`));
-    }
-
-    if (isLoadMock) require(mock)(app, opts);
-
+(async() => {
     await app.listen(opts.port, opts.transPort);
     kit.logs('dev server listen at:', br.cyan(opts.port));
 
